72401c22a48a78035185a6acc585fa0cf5b167b3
jw59 <junw59@outlook.com>
2021-01-16 16:33:46 +0800

    add first doc

--- myfirst/myfirst.cpp
+++ myfirst/myfirst.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     using namespace std;
-    cout << "Come up and c++ me some time.";
+    cout << "Come up and C++ me some time.";
     cout << endl;
     cout << "You won't regret it!" << endl;
     return 0;
